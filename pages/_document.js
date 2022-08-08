d8ef7b7a4ac1b8e14d363d1e1c9833decd4063c5
Nugie Jaya Nugraha <Nugie Jaya Nugraha>
2022-08-08 19:36:47 +0700

    add head

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <title>Team T2</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <link rel="icon" href="/img/gaming.jpg" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
