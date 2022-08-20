0b77d81bea8adf7d84eae8625daa56961b727727
Nugie Jaya Nugraha <Nugie Jaya Nugraha>
2022-08-20 11:07:00 +0700

    update func players

--- pages/players.js
+++ pages/players.js
@@ -1,62 +1,55 @@
 import HomeNav from "../components/navbar/HomeNav";
-import axios from 'axios';
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-class Players extends React.Component {
+function Players() {
+  const [post, setPost] = useState(null);
 
-    constructor() {
-        super()
-        this.state = {
-            users: []
-        }
-    }
+  useEffect(() => {
+    axios.get(`http://localhost:4000/players`).then((res) => {
+      setPost({
+        res: res.data,
+      });
+      console.log(res.data);
+    });
+  }, []);
+  
+  if (!post) return null;
 
-    componentWillMount() {
-        const url = "http://localhost:4000/players"
-        axios.get(url)
-            .then(users => {
-                console.log(users.data);
-                this.setState({
-                    users: users.data
-                })
-            })
-    }
-
-    render() {
-        return (
-            <>
-                <HomeNav />
-                <div className="container w-50 mt-3">
-                    <nav className="navbar navbar-light bg-light">
-                        <div className="container">
-                            <a className="navbar-brand">Players</a>
-                        </div>
-                    </nav>
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Username</th>
-                                <th scope="col">Role</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.users.map((user, idx) => {
-                                return (
-                                    <tr>
-                                        <th key={idx} scope="row">{user.id}</th>
-                                        <td key={idx}>{user.username}</td>
-                                        <td key={idx}>{user.role}</td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-            </>
-        )
-    }
+  return (
+    <>
+      <HomeNav />
+      <div className="container w-50 mt-3">
+        <nav className="navbar navbar-light bg-light">
+          <div className="container">
+            <a className="navbar-brand">Players</a>
+          </div>
+        </nav>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Username</th>
+              <th scope="col">Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {post.res.map((user, idx) => {
+              return (
+                <tr>
+                  <th key={idx} scope="row">
+                    {user.id}
+                  </th>
+                  <td key={idx}>{user.username}</td>
+                  <td key={idx}>{user.role}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
-
 
 export default Players;
